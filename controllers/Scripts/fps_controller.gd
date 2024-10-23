a8326f749830a535a042801de433ed551e810f6f
Nel <neltharak.sgl@gmail.com>
2024-10-23 14:59:44 +0200

    some logic changes, extra stuff, double jump doesnt work yet

--- controllers/Scripts/fps_controller.gd
+++ controllers/Scripts/fps_controller.gd
@@ -1,14 +1,59 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 10.0
+const JUMP_VELOCITY = 7.0
+
+@export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
+@export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
+@export var CAMERA_CONTROLLER : Camera3D
+
+
+var _mouse_input : bool = false
+var _mouse_rotation : Vector3
+var _rotation_input : float
+var _tilt_input : float
+var _can_double_jump : bool = false
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
+
+func _unhandled_input(event: InputEvent) -> void:
+	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	if _mouse_input:
+		_rotation_input = -event.relative.x
+		_tilt_input = -event.relative.y
+		print(Vector2(_rotation_input, _tilt_input))
+
+func _update_camera(delta):
+	_mouse_rotation.x += _tilt_input * delta
+	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	_mouse_rotation.y += _rotation_input * delta
+
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_mouse_rotation)
+	CAMERA_CONTROLLER.rotation.z = 0.0
+	_rotation_input = 0.0
+	_tilt_input = 0.0
+
+func _can_jump() -> bool:
+	if is_on_floor():
+		return true
+	
+	return false
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+	_update_camera(delta)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
